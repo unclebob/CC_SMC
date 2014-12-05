1c83b20f75e1435dbe749bb49e57ec39ccb401d1
Robert C. Martin <unclebob@cleancoder.com>
2014-12-05 16:45:27 -0600

    a little refactoring of SMC

--- src/smc/SMC.java
+++ src/smc/SMC.java
@@ -41,6 +41,9 @@ public class SMC {
     private String javaPackage = null;
     private String outputDirectory = null;
     private String language = "Java";
+    private SyntaxBuilder syntaxBuilder;
+    private Parser parser;
+    private Lexer lexer;
 
     public SmcCompiler(String[] args, Args argParser) {
       this.args = args;
@@ -48,62 +51,89 @@ public class SMC {
     }
 
     public void run() throws IOException {
+      extractCommandLineArguments();
+
+      FsmSyntax fsm = compile(getSourceCode());
+      int syntaxErrorCount = reportSyntaxErrors(fsm);
+
+      if (syntaxErrorCount == 0)
+        generateCode(fsm);
+    }
+
+    private void extractCommandLineArguments() {
       if (argParser.has('p'))
         javaPackage = argParser.getString('p');
       if (argParser.has('o'))
         outputDirectory = argParser.getString('o');
       if (argParser.has('l'))
         language = argParser.getString('l');
+    }
 
+    private String getSourceCode() throws IOException {
       String sourceFileName = args[argParser.nextArgument()];
-      String smContent = new String(Files.readAllBytes(Paths.get(sourceFileName)));
+      return new String(Files.readAllBytes(Paths.get(sourceFileName)));
+    }
 
-      SyntaxBuilder syntaxBuilder = new SyntaxBuilder();
-      Parser parser = new Parser(syntaxBuilder);
-      Lexer lexer = new Lexer(parser);
-      SemanticAnalyzer analyzer = new SemanticAnalyzer();
-      Optimizer optimizer = new Optimizer();
-      NSCGenerator generator = new NSCGenerator();
-
+    private FsmSyntax compile(String smContent) {
+      syntaxBuilder = new SyntaxBuilder();
+      parser = new Parser(syntaxBuilder);
+      lexer = new Lexer(parser);
       lexer.lex(smContent);
       parser.handleEvent(EOF, -1, -1);
 
-      FsmSyntax fsm = syntaxBuilder.getFsm();
-      int syntaxErrorCount = fsm.errors.size();
+      return syntaxBuilder.getFsm();
+    }
 
+    private int reportSyntaxErrors(FsmSyntax fsm) {
+      int syntaxErrorCount = fsm.errors.size();
       System.out.println(String.format("Compiled with %d syntax error%s.", syntaxErrorCount, (syntaxErrorCount == 1 ? "" : "s")));
 
       for (FsmSyntax.SyntaxError error : fsm.errors)
         System.out.println(error.toString());
+      return syntaxErrorCount;
+    }
 
-      if (syntaxErrorCount == 0) {
-        AbstractSyntaxTree ast = analyzer.analyze(fsm);
-        smc.StateMachine stateMachine = optimizer.optimize(ast);
+    private void generateCode(FsmSyntax fsm) throws IOException {
+      StateMachine stateMachine = optimize(fsm);
 
-        if (language.equalsIgnoreCase("java")) {
-          JavaNestedSwitchCaseImplementer implementer = new JavaNestedSwitchCaseImplementer(javaPackage);
-          generator.generate(stateMachine).accept(implementer);
+      NSCGenerator generator = new NSCGenerator();
+      if (language.equalsIgnoreCase("java"))
+        generateJava(stateMachine, generator);
+       else if (language.equalsIgnoreCase("c"))
+        generateC(stateMachine, generator);
+    }
 
-          String outputFileName = stateMachine.header.fsm + ".java";
+    private void generateC(StateMachine stateMachine, NSCGenerator generator) throws IOException {
+      CNestedSwitchCaseImplementer implementer = new CNestedSwitchCaseImplementer();
+      generator.generate(stateMachine).accept(implementer);
+      if (implementer.getErrors().size() > 0) {
+        for (CNestedSwitchCaseImplementer.Error error : implementer.getErrors())
+          System.out.println("Implementation error: " + error.name());
+      } else {
+        String outputFilePrefix = stateMachine.header.fsm.toLowerCase();
+        String headerFileName = outputFilePrefix + ".h";
+        String implementationFileName = outputFilePrefix + ".c";
 
-          Files.write(getOutputPath(outputFileName), implementer.getOutput().getBytes());
-        } else if (language.equalsIgnoreCase("c")) {
-          CNestedSwitchCaseImplementer implementer = new CNestedSwitchCaseImplementer();
-          generator.generate(stateMachine).accept(implementer);
-          if (implementer.getErrors().size() > 0) {
-            for (CNestedSwitchCaseImplementer.Error error : implementer.getErrors())
-              System.out.println("Implementation error: " + error.name());
-          } else {
-            String outputFilePrefix = stateMachine.header.fsm.toLowerCase();
-            String headerFileName = outputFilePrefix + ".h";
-            String implementationFileName = outputFilePrefix + ".c";
-
-            Files.write(getOutputPath(headerFileName), implementer.getFsmHeader().getBytes());
-            Files.write(getOutputPath(implementationFileName), implementer.getFsmImplementation().getBytes());
-          }
-        }
-
+        Files.write(getOutputPath(headerFileName), implementer.getFsmHeader().getBytes());
+        Files.write(getOutputPath(implementationFileName), implementer.getFsmImplementation().getBytes());
       }
+    }
+
+    private void generateJava(StateMachine stateMachine, NSCGenerator generator) throws IOException {
+      JavaNestedSwitchCaseImplementer implementer = new JavaNestedSwitchCaseImplementer(javaPackage);
+      generator.generate(stateMachine).accept(implementer);
+
+      String outputFileName = stateMachine.header.fsm + ".java";
+
+      Files.write(getOutputPath(outputFileName), implementer.getOutput().getBytes());
+    }
+
+    private StateMachine optimize(FsmSyntax fsm) {
+      SemanticAnalyzer analyzer = new SemanticAnalyzer();
+      Optimizer optimizer = new Optimizer();
+
+      AbstractSyntaxTree ast = analyzer.analyze(fsm);
+      return optimizer.optimize(ast);
     }
 
     private Path getOutputPath(String outputFileName) {
